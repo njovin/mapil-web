129f136658ece4892a33774111ad57eee6feb1a8
Nathan Jovin <njovin@zingleme.com>
2016-04-11 07:28:13 -0700

    Email addl validation

--- app/Models/EmailAddress.php
+++ app/Models/EmailAddress.php
@@ -5,7 +5,8 @@ namespace Mapil\Models;
 class EmailAddress extends Model
 {
     protected $rules = array(
-        'email' => ['email',"regex:/[a-zA-Z0-9_]*@email.mapil.co/"],
+        'email' => ['email',"required","regex:/[a-zA-Z0-9_]*@email.mapil.co/"],
+        'user_id' => ['integer']
     );
     protected $messages = [
         'email.regex' => "Email address must end in email.mapil.co"
