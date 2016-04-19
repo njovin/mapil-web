afffe869f258ac014a0fbc7d14cf8edd6fe64446
Nathan Jovin <njovin@zingle.me>
2016-04-19 09:39:12 -0700

    More menu fixes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,31 +1,26 @@
     $(document).ready(function() {
         var secureMenuToggle = $("#js-mobile-menu").unbind();
-            $("#js-navigation-menu").removeClass("show");
+        $("#js-navigation-menu").removeClass("show");
             
-            secureMenuToggle.on("click", function(e) {
+        secureMenuToggle.on("click", function(e) {
             
             e.preventDefault();
             
             $("#js-navigation-menu").slideToggle(function(){
-            
-            if($("#js-navigation-menu").is(":hidden")) {
-            $("#js-navigation-menu").removeAttr("style");
-            }
+                if($("#js-navigation-menu").is(":hidden")) {
+                    $("#js-navigation-menu").removeAttr("style");
+                }
             });
         });
 
-        var insecureMenuToggle = $("#js-centered-navigation-mobile-men").unbind();
-            $("#js-centered-navigation-menu").removeClass("show");
-            
-            insecureMenuToggle.on("click", function(e) {
-            
+        var insecureMenuToggle = $("#js-centered-navigation-mobile-menu").unbind();
+        $("#js-centered-navigation-menu").removeClass("show");
+        insecureMenuToggle.on("click", function(e) {
             e.preventDefault();
-            
             $("#js-centered-navigation-menu").slideToggle(function(){
-            
-            if($("#js-centered-navigation-menu").is(":hidden")) {
-            $("#js-centered-navigation-menu").removeAttr("style");
-            }
+                if($("#js-centered-navigation-menu").is(":hidden")) {
+                    $("#js-centered-navigation-menu").removeAttr("style");
+                }
             });
         });            
     }); 
