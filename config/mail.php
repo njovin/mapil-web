f14ee1499de15376a0e40ce6282cbce4f4f33ab7
Nathan Jovin <njovin@zingleme.com>
2017-02-18 23:47:53 -0800

    Add default email sender address

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@mapil.co', 'name' => null],
 
     /*
     |--------------------------------------------------------------------------
