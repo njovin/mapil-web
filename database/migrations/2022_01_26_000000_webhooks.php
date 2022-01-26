841e431d2b5589c7d90acf3f717ceb0cbf9a1bd4
Nathan Jovin <nathanjovin@gmail.com>
2022-01-26 07:13:35 -0800

    Webhook migration

---
+++ database/migrations/2022_01_26_000000_webhooks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Webhooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->boolean('webhooks_enabled')->default(false);
+            $table->string('webhook_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('webhooks_enabled');
+            $table->dropColumn('webhook_url');
+        });
+    }
+}
